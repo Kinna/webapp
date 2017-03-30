7c4264bef7af149f1e9e6a5f5f052401455bc316
Kinna <kinna.mk@gmail.com>
2017-03-30 13:41:48 +0200

    Create database create file

---
+++ database/create.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Use this file to build the database structure
+ */
+
+require_once('../libs/custom/database/database-includer.php');
+use Database\Database;
+
+$db = new Database();
