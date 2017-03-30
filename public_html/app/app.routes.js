3fd3786895ae248432da12f2c0382e55207c8cee
Kinna <kinna.mk@gmail.com>
2017-03-30 14:59:21 +0200

    Created default route file

---
+++ public_html/app/app.routes.js
@@ -0,0 +1,6 @@
+angular.module('app').config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'app/core/default.html'
+        });
+}]);
