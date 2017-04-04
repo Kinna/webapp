0926201cd55ad73cc079e525607e2834b80db0cb
kinna.mk@gmail.com <BWRAF2re>
2017-04-04 14:29:21 +0200

    Minimized console output

--- public_html/modules/api/api.service.js
+++ public_html/modules/api/api.service.js
@@ -8,20 +8,17 @@ angular.module('api').factory('ApiService', ['$http', function($http){
 	// The callback onResponse has two arguments: error and data
 	service.get = function(url, onResponse){
 		$http.get(url).then(function(response){
-			if(response.data.error)	console.error('POST ' + url + ': ' + response.data.error);
 			onResponse(response.data.error, response.data.data);
 		}, function(response){
-			console.error('POST ' + url + ': HTTP error');
+			console.error(response);
 			onResponse('HTTP error', null);
 		});
 	};
 
 	service.post = function(url, data, onResponse){
 		$http.post(url, angular.toJson(data)).then(function(response){
-			if(response.data.error)	console.error('POST ' + url + ': ' + response.data.error);
 			onResponse(response.data.error, response.data.data);
 		}, function(response){
-			console.error('POST ' + url + ': HTTP error');
 			console.error(response);
 			onResponse('HTTP error', null);
 		});
