c373b6bf197f46290c188334c18de72b2f44b124
Kinna <kinna.mk@gmail.com>
2017-04-14 11:54:57 +0200

    Delete app.routes.js

--- public_html/app/app.routes.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'app/core/default.html'
-        });
-}]);
